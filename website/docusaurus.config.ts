8d645ee810ebb7f81202b651afe29a89962c6ba1
Rabindra184 <46239257+Rabindra184@users.noreply.github.com>
2026-02-22 22:55:31 +0530

    chore(website): update production URL for Cloudflare Pages

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://appium-xenon-eight.vercel.app',
+  url: 'https://xenon-6e6.pages.dev',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
